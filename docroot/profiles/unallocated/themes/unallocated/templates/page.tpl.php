419971ca83f3bee045999771e8c703522cd26763
Michael <michael@runninggun.com>
2014-12-18 16:20:14 -0500

    [#244] New Theme updated, Menu restored to minimal functionality, basic media styles

--- docroot/profiles/unallocated/themes/unallocated/templates/page.tpl.php
+++ docroot/profiles/unallocated/themes/unallocated/templates/page.tpl.php
@@ -2,50 +2,42 @@
 //Container Declaration
 
 $third = '"one-third columns"';
-$fullWidthNav = '"nav sixteen columns"';
 $fullWidth = '"sixteen columns"';
 $fullWidthNav = '"nav sixteen columns"';
 $ColumnAWidth = '"five columns alpha"';
 $ColumnBWidth = '"eleven columns omega"';
 
 //Prints Heading based on Values
+print ("<div class= 'top_zone'>");
 print ("<div class= 'container'>");
-
-print ("<div class= $third>");
-//print render($page['branding']);
-print ("</div>");
-
-print ("<div class= $third>");
 if ($logo): ?>
       <a href="<?php print $front_page; ?>" title="<?php  print t('Home'); ?>" rel="home" id="logo">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+        <img id="logoImg", src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
       </a>
     <?php endif; ?>
     
  <?php
-print ("</div>");
-
-
-print ("<div class= $third>");
-	print render($page['user_bar_first']);		
 print ("</div>");
 print ("</div>");
 print ("<div class= 'container'>");
 
 
 //Special div for Navigation elements
-print ("<div class= $fullWidthNav>");
-//print render($page['menu']);
+print ("<div class = 'container'>");
+print ("<div class = 'container'>");
+	print render($page['menu']);
+print ("</div>");
 print ("</div>");
 
 //Main Content section, with Sidebar relivence check
 
 print ("</div>");
 
-print ("<div class= 'core container'>");
+print ("<div class= $fullWidth>");
+print ("<div class= 'core'>");
+print ("<div class= container>");
 
  if($page['sidebar_first']) {
-	print ("<div class= container>");
 	print ("<div class= $ColumnAWidth>");  
 	print render($page['sidebar_first']);
 	print ("</div>");
@@ -53,21 +45,20 @@ print ("<div class= 'core container'>");
 	print ("<div class= $ColumnBWidth>");  	
 	print render($page['content']);	
 	print ("</div>");
-	
-print ("</div>");
  }
  
  else
- 	{
-	print ("<div class= $fullWidth>");  	
+ 	{ 	
 	print render($page['content']);	
-	print ("</div>");	
 	}
-
+	
+print ("</div>");
+print ("</div>");
 print ("</div>");
     
 //Closing Sections.
 
+print ("<div class= 'bottom_zone'>");
 print ("<div class= 'container'>");
 
 	print ("<div class= $fullWidth>");
@@ -78,6 +69,6 @@ print ("<div class= 'container'>");
 	print ("<div class= $fullWidth>");
 	print render($page['post_postscript']);
 	print ("</div>");
-
+	print ("</div>");
 	print ("</div>");
 ?>  
